7336c16940f1fb993c427ff3e4bf18eb6f73a9c0
Arael <arael.roca@gmail.com>
2022-11-11 09:48:37 +0100

    Start

---
+++ PythonABC.py
@@ -0,0 +1,30 @@
+#Variable and Strings
+
+#print('Hello World')
+
+var = 'Hello World'
+
+newvar = 'amigos'
+
+#print(f'{var} {newvar}')
+
+#print(var)
+
+
+#Lists
+bikes = []
+bikes.append('trek')
+bikes.append('redline')
+bikes. append('giant')
+
+
+#for bike in bikes:
+    #print(bike)
+
+numbers = []
+
+for number in range (1, 10):
+    numbers.append(number**2)
+
+for number in numbers:
+    print(number)
